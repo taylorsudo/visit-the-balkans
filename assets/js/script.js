576119815ce1274e0ffa3cb6f601a7f6ba1ca43d
Taylor Lee <126066949+taylorsudo@users.noreply.github.com>
2023-11-15 05:46:16 +1100

    Add code comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 /* Side Navigation
 Adapted from How TO - Side Navigation by W3Schools
 https://www.w3schools.com/howto/howto_js_sidenav.asp */
+
+// Set the default width of the side navigation to 25rem
 function openNav() {
   document.getElementById("nav").style.width = "25rem";
 }
 
-/* Set the width of the side navigation to 0 */
+// Set the width of the side navigation to 0
 function closeNav() {
   document.getElementById("nav").style.width = "0";
 }
