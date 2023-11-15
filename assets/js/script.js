647dd925f2d1e68c86814b3731b0162ab527ac4d
Taylor Lee <126066949+taylorsudo@users.noreply.github.com>
2023-11-16 01:45:07 +1100

    Smoother side navigation animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,12 @@ https://www.w3schools.com/howto/howto_js_sidenav.asp */
 
 // Set the default width of the side navigation to 25rem
 function openNav() {
-  document.getElementById("nav").style.width = "25rem";
+  document.getElementById("nav").style.right = "0";
 }
 
 // Set the width of the side navigation to 0
 function closeNav() {
-  document.getElementById("nav").style.width = "0";
+  document.getElementById("nav").style.right = "-25rem";
 }
 
 /* Back to Top
