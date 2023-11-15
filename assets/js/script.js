75992938d6bad280a17452f2ab8890a44666ec28
Taylor Lee <126066949+taylorsudo@users.noreply.github.com>
2023-11-15 13:08:52 +1100

    Add back to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,31 @@ function openNav() {
 function closeNav() {
   document.getElementById("nav").style.width = "0";
 }
+
+/* Back to Top
+Adapted from Bootstrap Scroll To Top Button
+https://mdbootstrap.com/docs/standard/extended/back-to-top/ */ 
+document.addEventListener("DOMContentLoaded", function () {
+  let mybutton = document.getElementById("top");
+
+  // When the user scrolls down 100px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      mybutton.classList.add("visible");
+    } else {
+      mybutton.classList.remove("visible");
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+});
